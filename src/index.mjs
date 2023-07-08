f4eb77b45c51bc8b0a82e2e4ecb8d02a4e09fdad
msdio <rlagofka1@gmail.com>
2023-07-08 21:42:47 +0900

    feat: create names and prefix array

---
+++ src/index.mjs
@@ -0,0 +1,5 @@
+import { ANIMAL_NAMES } from "./names.mjs";
+import { PREFIX } from "./prefix.mjs";
+
+console.log("total", ANIMAL_NAMES.length, PREFIX.length);
+console.log("tot", ANIMAL_NAMES.length * PREFIX.length);
